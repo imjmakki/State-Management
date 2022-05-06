d03edc49b56a49a3073650da72e8ab0010bec74f
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-06 14:20:11 +0300

    State Management

--- lib/screen/task.dart
+++ lib/screen/task.dart
@@ -86,14 +86,21 @@ class TaskList extends StatelessWidget {
             onChanged: (bool? value) {},
           ),
         ),
-        ListTile(
-          title: Text('This is a task.'),
-          trailing: Checkbox(
-            value: false,
-            onChanged: (bool? value) {},
-          ),
-        ),
+        TaskTile(),
       ],
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('This is a task.'),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (bool? value) {},
+      ),
     );
   }
 }
