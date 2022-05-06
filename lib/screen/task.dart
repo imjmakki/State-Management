3464838bd75117540661b53bbe1c871f354f9bbd
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-06 13:16:09 +0300

    State Management

--- lib/screen/task.dart
+++ lib/screen/task.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class Task extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: [
+          Icon(Icons.list),
+        ],
+      ),
+    );
   }
 }
