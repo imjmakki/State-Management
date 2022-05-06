006e06abf48e08863f061c8cce12d30ff4ac9060
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-06 14:49:18 +0300

    State Management

--- lib/screen/add_task.dart
+++ lib/screen/add_task.dart
@@ -5,6 +5,8 @@ class AddTask extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color(0xff757575),
+    );
   }
 }
