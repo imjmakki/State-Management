4b0d3422dc6be71c025863a7c920233606ac927f
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-06 13:06:34 +0300

    State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:state/screen/task.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Task(),
+    );
   }
 }
