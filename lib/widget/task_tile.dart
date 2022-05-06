e218789c3a23712b4e3f46d28448c0999e6670e7
Mohamad J. Makki <malanbaki@transforat.com>
2022-05-06 17:38:34 +0300

    State Management

--- lib/widget/task_tile.dart
+++ lib/widget/task_tile.dart
@@ -5,11 +5,18 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task.'),
-      trailing: Checkbox(
-        activeColor: Colors.lightBlueAccent,
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      trailing: CheckBox(),
+    );
+  }
+}
+
+class CheckBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: false,
+      onChanged: (bool? value) {},
     );
   }
 }
